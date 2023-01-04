1dcaa95694e4467b06d6d0895c357c8889e17492
whistleJs <whistle_js@naver.com>
2023-01-04 15:15:32 +0900

    feat(client): 폰트 추가 및 적용

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,6 +1,13 @@
-import "../styles/globals.css";
+import { Global } from "@emotion/react";
 import type { AppProps } from "next/app";
 
+import { GlobalStyles } from "@/styles/global.style";
+
 export default ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global styles={GlobalStyles} />
+      <Component {...pageProps} />
+    </>
+  );
 };
---
+++ packages/client/styles/global.style.ts
@@ -0,0 +1,17 @@
+import { css } from "@emotion/react";
+import localFont from "@next/font/local";
+
+const BazziFont = localFont({
+  src: "./fonts/Bazzi.woff",
+  weight: "400",
+});
+
+export const GlobalStyles = css`
+  * {
+    ${BazziFont.style}
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+  }
+`;
