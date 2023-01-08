ee4988a1189984c6cf5da09b9d2ef0f86af01e86
whistleJs <whistle_js@naver.com>
2023-01-08 23:32:32 +0900

    feat(client): 로그인 페이지 URL 변경

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,33 +1,3 @@
-import { FlexStyles } from "@/styles/common/flex.style";
-import { ContainerStyles } from "@/styles/page/index.style";
-
-import CommonButton from "@/components/common/common-button";
-import CommonInput from "@/components/common/common-input";
-
 export default () => {
-  return (
-    <ContainerStyles column alignItems="center" justifyContent="center">
-      <FlexStyles column>
-        <span className="title">
-          <sup>Just</sup>Chatting
-        </span>
-
-        <FlexStyles column className="form">
-          <FlexStyles column className="input-groups">
-            <CommonInput title="아이디" placeholder="ID" />
-            <CommonInput
-              type="password"
-              title="비밀번호"
-              placeholder="Password"
-            />
-          </FlexStyles>
-
-          <FlexStyles gap="10px" className="button-groups">
-            <CommonButton>로그인</CommonButton>
-            <CommonButton>회원가입</CommonButton>
-          </FlexStyles>
-        </FlexStyles>
-      </FlexStyles>
-    </ContainerStyles>
-  );
+  return <></>;
 };
---
+++ packages/client/pages/sign-in.tsx
@@ -0,0 +1,33 @@
+import CommonButton from "@/components/common/common-button";
+import CommonInput from "@/components/common/common-input";
+
+import { FlexStyles } from "@/styles/common/flex.style";
+import { ContainerStyles } from "@/styles/page/sign-in.style";
+
+export default () => {
+  return (
+    <ContainerStyles column alignItems="center" justifyContent="center">
+      <FlexStyles column>
+        <span className="title">
+          <sup>Just</sup>Chatting
+        </span>
+
+        <FlexStyles column className="form">
+          <FlexStyles column className="input-groups">
+            <CommonInput title="아이디" placeholder="ID" />
+            <CommonInput
+              type="password"
+              title="비밀번호"
+              placeholder="Password"
+            />
+          </FlexStyles>
+
+          <FlexStyles gap="10px" className="button-groups">
+            <CommonButton>로그인</CommonButton>
+            <CommonButton>회원가입</CommonButton>
+          </FlexStyles>
+        </FlexStyles>
+      </FlexStyles>
+    </ContainerStyles>
+  );
+};
