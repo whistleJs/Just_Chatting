b0eabd6f0bcbac61ef435c9feafae37bb84a8777
whistleJs <whistle_js@naver.com>
2023-01-09 20:10:49 +0900

    feat(client): 회원가입 페이지 개발 진행중

---
+++ packages/client/core/constants/account.constants.ts
@@ -0,0 +1,29 @@
+export const NAME_REGEX = "^[a-zA-Z가-힣 ]{2,16}$";
+export const NICKNAME_REGEX = "^[a-zA-Z가-힣0-9 ]{2,20}$";
+export const EMAIL_REGEX = "^[a-zA-Z0-9-_]+@[a-z-]+\\.[a-z]{2,3}$";
+export const PASSWORD_REGEX = "^(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*()\\-_])[\\da-zA-Z!@#$%^&*()\\-_]{8,16}$";
+
+export const NAME_ERROR_MESSAGE = {
+  INVALID: "이름 형식이 유효하지 않습니다.",
+};
+export const NICKNAME_ERROR_MESSAGE = {
+  INVALID: "닉네임 형식이 유효하지 않습니다.",
+  DUPLICATE: "이미 존재하는 닉네임입니다.",
+};
+export const EMAIL_ERROR_MESSAGE = {
+  INVALID: "이메일 형식이 유효하지 않습니다.",
+  DUPLICATE: "이미 존재하는 이메일입니다.",
+};
+export const PASSWORD_ERROR_MESSAGE = {
+  INVALID: "비밀번호 형식이 유효하지 않습니다.",
+};
+export const PASSWORD_CONFIRM_ERROR_MESSAGE = {
+  INVALID: "비밀번호 형식이 유효하지 않습니다.",
+  INCORRECT: "비밀번호가 일치하지 않습니다.",
+};
+
+export type NAME_ERROR_CODE = keyof typeof NAME_ERROR_MESSAGE;
+export type NICKNAME_ERROR_CODE = keyof typeof NICKNAME_ERROR_MESSAGE;
+export type EMAIL_ERROR_CODE = keyof typeof EMAIL_ERROR_MESSAGE;
+export type PASSWORD_ERROR_CODE = keyof typeof PASSWORD_ERROR_MESSAGE;
+export type PASSWORD_CONFIRM_ERROR_CODE = keyof typeof PASSWORD_CONFIRM_ERROR_MESSAGE;
--- packages/client/pages/sign-up.tsx
+++ packages/client/pages/sign-up.tsx
@@ -1,38 +1,153 @@
 import Link from "next/link";
+import { useState } from "react";
 
 import CommonButton from "@/components/common/common-button";
 import CommonInput from "@/components/common/common-input";
+
+import {
+  EMAIL_ERROR_CODE,
+  EMAIL_ERROR_MESSAGE,
+  EMAIL_REGEX,
+  NAME_ERROR_CODE,
+  NAME_ERROR_MESSAGE,
+  NAME_REGEX,
+  NICKNAME_ERROR_CODE,
+  NICKNAME_ERROR_MESSAGE,
+  NICKNAME_REGEX,
+  PASSWORD_CONFIRM_ERROR_CODE,
+  PASSWORD_CONFIRM_ERROR_MESSAGE,
+  PASSWORD_ERROR_CODE,
+  PASSWORD_ERROR_MESSAGE,
+  PASSWORD_REGEX,
+} from "@/core/constants/account.constants";
+import useValidate from "@/core/hooks/useValidate";
 
 import SignLayout from "@/layouts/sign";
 
 import { FlexStyles } from "@/styles/common/flex.style";
 
 export default () => {
+  const [nameErrorCode, setNameErrorCode] = useState<NAME_ERROR_CODE | null>(null);
+  const [nicknameErrorCode, setNicknameErrorCode] = useState<NICKNAME_ERROR_CODE | null>(null);
+  const [emailErrorCode, setEmailErrorCode] = useState<EMAIL_ERROR_CODE | null>(null);
+  const [passwordErrorCode, setPasswordErrorCode] = useState<PASSWORD_ERROR_CODE | null>(null);
+  const [passwordConfirmErrorCode, setPasswordConfirmErrorCode] = useState<PASSWORD_CONFIRM_ERROR_CODE | null>(null);
+
+  const [name, isInvalidName, setName] = useValidate(NAME_REGEX);
+  const [nickname, isInvalidNickname, setNickname] = useValidate(NICKNAME_REGEX);
+  const [email, isInvalidEmail, setEmail] = useValidate(EMAIL_REGEX);
+  const [password, isInvalidPassword, setPassword] = useValidate(PASSWORD_REGEX);
+  const [passwordConfirm, isInvalidPasswordConfirm, setPasswordConfirm] = useValidate(PASSWORD_REGEX);
+
+  const validate = () => {
+    let isValidate = true;
+
+    if (isInvalidName) {
+      setNameErrorCode("INVALID");
+      isValidate = false;
+    } else {
+      setNameErrorCode(null);
+    }
+
+    if (isInvalidNickname) {
+      setNicknameErrorCode("INVALID");
+      isValidate = false;
+    } else {
+      setNicknameErrorCode(null);
+    }
+
+    if (isInvalidEmail) {
+      setEmailErrorCode("INVALID");
+      isValidate = false;
+    } else {
+      setEmailErrorCode(null);
+    }
+
+    if (isInvalidPassword) {
+      setPasswordErrorCode("INVALID");
+      isValidate = false;
+    } else {
+      setPasswordErrorCode(null);
+    }
+
+    if (isInvalidPasswordConfirm) {
+      setPasswordConfirmErrorCode("INVALID");
+      isValidate = false;
+    } else if (password !== passwordConfirm) {
+      setPasswordConfirmErrorCode("INCORRECT");
+      isValidate = false;
+    } else {
+      setPasswordConfirmErrorCode(null);
+    }
+
+    return isValidate;
+  };
+
+  const handlerSignUp = () => {
+    if (!validate()) return;
+
+    console.log("Validate Success");
+  };
+
   return (
     <SignLayout>
       <FlexStyles column className="form">
         <FlexStyles column className="input-groups">
-          <CommonInput title="이름" placeholder="Name" />
+          <CommonInput
+            title="이름"
+            placeholder="Name"
+            value={name}
+            text={nameErrorCode && NAME_ERROR_MESSAGE[nameErrorCode]}
+            isError={nameErrorCode !== null}
+            onChange={(e) => setName(e.target.value)}
+            onEnter={handlerSignUp}
+          />
 
-          <CommonInput title="닉네임" placeholder="Nickname" />
+          <CommonInput
+            title="닉네임"
+            placeholder="Nickname"
+            value={nickname}
+            text={nicknameErrorCode && NICKNAME_ERROR_MESSAGE[nicknameErrorCode]}
+            isError={nicknameErrorCode !== null}
+            onChange={(e) => setNickname(e.target.value)}
+            onEnter={handlerSignUp}
+          />
 
-          <CommonInput title="아이디" placeholder="ID" />
+          <CommonInput
+            title="이메일"
+            placeholder="Email"
+            value={email}
+            text={emailErrorCode && EMAIL_ERROR_MESSAGE[emailErrorCode]}
+            isError={emailErrorCode !== null}
+            onChange={(e) => setEmail(e.target.value)}
+            onEnter={handlerSignUp}
+          />
 
           <CommonInput
             type="password"
             title="비밀번호"
             placeholder="Password"
+            value={password}
+            text={passwordErrorCode && PASSWORD_ERROR_MESSAGE[passwordErrorCode]}
+            isError={passwordErrorCode !== null}
+            onChange={(e) => setPassword(e.target.value)}
+            onEnter={handlerSignUp}
           />
 
           <CommonInput
             type="password"
             title="비밀번호 확인"
             placeholder="Password Confirm"
+            value={passwordConfirm}
+            text={passwordConfirmErrorCode && PASSWORD_CONFIRM_ERROR_MESSAGE[passwordConfirmErrorCode]}
+            isError={passwordConfirmErrorCode !== null}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+            onEnter={handlerSignUp}
           />
         </FlexStyles>
 
         <FlexStyles className="button-groups">
-          <CommonButton>회원가입</CommonButton>
+          <CommonButton onClick={handlerSignUp}>회원가입</CommonButton>
         </FlexStyles>
       </FlexStyles>
 
