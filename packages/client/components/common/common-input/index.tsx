d1772e564756fdc374d7c626f095d1163a11c2e2
whistleJs <whistle_js@naver.com>
2023-01-09 20:10:05 +0900

    feat(client): common-input 수정
    - onKeyDown, onEnter 이벤트 추가
    - isError 속성 추가
    - 스타일 추가

--- packages/client/components/common/common-input/index.tsx
+++ packages/client/components/common/common-input/index.tsx
@@ -1,27 +1,34 @@
-import { ChangeEvent, HTMLInputTypeAttribute } from "react";
+import { ChangeEvent, HTMLInputTypeAttribute, KeyboardEvent } from "react";
 
 import { CommonInputStyles } from "./style";
 
 type CommonInputProps = {
   type?: HTMLInputTypeAttribute;
   title?: JSX.Element | string;
-  text?: JSX.Element | string;
+  text?: JSX.Element | string | null;
   placeholder?: string;
   value?: string | string[] | number;
 
+  isError?: boolean;
+
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  onEnter?: (e: KeyboardEvent<HTMLInputElement>) => void;
+  onKeyDown?: (e: KeyboardEvent<HTMLInputElement>) => void;
 };
 
-export default ({
-  type,
-  title,
-  text,
-  placeholder,
-  value,
-  onChange,
-}: CommonInputProps) => {
+export default ({ type, title, text, placeholder, value, isError, onChange, onKeyDown, onEnter }: CommonInputProps) => {
+  const handlerKeydown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && onEnter) {
+      onEnter(e);
+    }
+
+    if (onKeyDown) {
+      onKeyDown(e);
+    }
+  };
+
   return (
-    <CommonInputStyles column>
+    <CommonInputStyles column isError={isError || false}>
       {title && <div className="title">{title}</div>}
 
       <input
@@ -29,6 +36,7 @@ export default ({
         placeholder={placeholder}
         value={value}
         onChange={onChange}
+        onKeyDown={handlerKeydown}
       />
 
       {text && <div className="text">{text}</div>}
--- packages/client/components/common/common-input/style.ts
+++ packages/client/components/common/common-input/style.ts
@@ -2,22 +2,26 @@ import styled from "@emotion/styled";
 
 import { FlexStyles } from "@/styles/common/flex.style";
 
-export const CommonInputStyles = styled(FlexStyles)`
-  width: 100%;
+import { CommonInputStylesProps } from "./style.model";
 
-  & > .title {
-    font-size: 16px;
-  }
+export const CommonInputStyles = styled(FlexStyles)<CommonInputStylesProps>(({ theme, isError }) => ({
+  width: "100%",
 
-  & > .text {
-    font-size: 14px;
-  }
+  "& > .title": {
+    fontSize: "16px",
+  },
 
-  & > input {
-    margin-top: 4px;
-    padding: 10px 8px;
-    width: 100%;
-    border: none;
-    border-radius: 4px;
-  }
-`;
+  "& > .text": {
+    fontSize: "14px",
+    color: `${isError ? theme.color.orange.default : "inherit"}`,
+  },
+
+  "& > input": {
+    margin: "6px 0 8px",
+    padding: "10px 8px",
+    width: "100%",
+    border: "none",
+    borderRadius: "4px",
+    outline: `solid ${theme.color[isError ? "red" : "main"].default}`,
+  },
+}));
