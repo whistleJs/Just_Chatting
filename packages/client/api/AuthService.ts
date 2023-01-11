6b1edafe530e92d83804b059113fb0325e2c2175
whistleJs <whistle_js@naver.com>
2023-01-11 22:49:01 +0900

    feat(client): AuthService 수정
    - request data model 생성

--- packages/client/api/AuthService.ts
+++ packages/client/api/AuthService.ts
@@ -2,14 +2,18 @@ import { AxiosRequestConfig } from "axios";
 
 import axios from "@/core/utils/axios.util";
 
-export default {
-  signUp: async (data: any) => {
-    const config: AxiosRequestConfig = {
-      method: "POST",
-      url: "/auth/sign-up",
-      data,
-    };
+import { AuthSignUpRequest } from "./model/auth.model";
 
-    return axios.request(config);
-  },
+const signUp = (data: AuthSignUpRequest) => {
+  const config: AxiosRequestConfig = {
+    method: "POST",
+    url: "/auth/sign-up",
+    data,
+  };
+
+  return axios.request(config);
+};
+
+export default {
+  signUp,
 };
---
+++ packages/client/api/model/auth.model.ts
@@ -0,0 +1,6 @@
+export interface AuthSignUpRequest {
+  email: string;
+  nickname: string;
+  password: string;
+  name: string;
+}
