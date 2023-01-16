a7961fcc934fd5fe3470edf22a1eb9135e162bfb
whistleJs <whistle_js@naver.com>
2023-01-16 19:54:18 +0900

    refactor(server): User Controller 수정
    - 불필요한 코드 수정

--- packages/server/src/api/controller/user.controller.ts
+++ packages/server/src/api/controller/user.controller.ts
@@ -1,15 +1,8 @@
-import { Controller, Get, Headers } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 import UserService from '@/api/service/user.service';
-import Users from '@/api/model/entity/Users.entity';
 
 @Controller('users')
 export default class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Get('count')
-  getCount(@Headers('user') requestUser: Users) {
-    console.log(requestUser, 'asdf');
-    return this.userService.getCount();
-  }
 }
