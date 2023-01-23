ea7bac23a22ea6a8abfa896f5394224757c14079
whistleJs <whistle_js@naver.com>
2023-01-23 21:49:20 +0900

    feat(client): Input 스타일 수정

--- packages/client/src/styles/components/input.style.ts
+++ packages/client/src/styles/components/input.style.ts
@@ -6,18 +6,24 @@ interface Props {
   status?: "default" | "error";
 }
 
-const borderStyles = {
-  default: `solid 1px ${ThemeColors.gray.default}`,
-  error: `solid 2px ${ThemeColors.red.default} !important`,
+const InputStyles = {
+  default: {
+    padding: "10px",
+    border: `solid 1px ${ThemeColors.gray.default}`,
+  },
+  error: {
+    padding: "9px",
+    border: `solid 2px ${ThemeColors.red.default} !important`,
+  },
 };
 
 export const Input = styled.input<Props>(({ status = "default" }) => ({
-  padding: "10px",
   width: "100%",
   borderRadius: "4px",
-  border: borderStyles[status],
+  ...InputStyles[status],
 
   "&:focus": {
+    padding: "9px",
     border: `solid 2px ${ThemeColors.main.default}`,
   },
 }));
