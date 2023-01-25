814409d5039ed4d35f5cfb824a5f9d57abdfb933
whistleJs <whistle_js@naver.com>
2023-01-25 20:41:00 +0900

    feat(client): 메인 페이지 접근 제한

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,4 +1,26 @@
+import { GetServerSideProps } from "next";
+import { useEffect } from "react";
+
+export const getServerSideProps: GetServerSideProps = async ({ req: { cookies } }) => {
+  if (cookies.token === undefined) {
+    return {
+      redirect: {
+        destination: "/sign-in",
+        permanent: true,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
+
 const IndexPage = () => {
+  useEffect(() => {
+    console.log("Mount");
+  }, []);
+
   return <></>;
 };
 
